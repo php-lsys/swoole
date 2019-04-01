3151109aae68676c5423d61671fd085d3fb84c48
shanliu <shanliu@msn.com>
2019-04-01 10:23:24 +0800

    .

--- src/classes/LSYS/Swoole/Thrift/Server/TBinaryProtocolAcceleratedFactory.php
+++ src/classes/LSYS/Swoole/Thrift/Server/TBinaryProtocolAcceleratedFactory.php
@@ -1,9 +1,18 @@
 <?php
 namespace LSYS\Swoole\Thrift\Server;
-class TBinaryProtocolAcceleratedFactory extends \Thrift\Factory\TBinaryProtocolFactory
+use Thrift\Factory\TProtocolFactory;
+use Thrift\Protocol\TBinaryProtocolAccelerated;
+class TBinaryProtocolAcceleratedFactory implements TProtocolFactory
 {
-  public function getProtocol($trans)
-  {
-      return new \Thrift\Protocol\TBinaryProtocolAccelerated($trans, $this->strictRead_, $this->strictWrite_);
-  }
+    private $strictRead_ = false;
+    private $strictWrite_ = false;
+    public function __construct($strictRead=false, $strictWrite=false)
+    {
+        $this->strictRead_ = $strictRead;
+        $this->strictWrite_ = $strictWrite;
+    }
+    public function getProtocol($trans)
+    {
+        return new TBinaryProtocolAccelerated($trans, $this->strictRead_, $this->strictWrite_);
+    }
 }
