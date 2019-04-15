8b2ce91bf80c1dd9aa599c96c84ee5e4cc055fbe
shanliu <shanliu@msn.com>
2019-04-15 11:10:35 +0800

    ..

--- dome_pool/config/swoole.php
+++ dome_pool/config/swoole.php
@@ -1,6 +1,14 @@
 <?php
 return array(
     "mysql"=>array(
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'user' => 'root',
+        'password' => '110',
+        'fetch_mode' 		=> 1,
+        'database' => 'test',
+     ),
+    "mysql_pool"=>array(
         "try"=>true,//发送错误重试次数,设置为TRUE为不限制
         "sleep"=>1,//断开连接重连暂停时间
         "master"=>array(
@@ -57,13 +65,20 @@ return array(
         ),
     ),
     "redis"=>array(
+      ///  'auth'              =>'sss',
+        'host'             	=> '192.168.1.101',
+        'port'             	=> 6379,
+        'timeout'			=> '60',
+        'db'				=> NULL,
+    ),
+    "redis_pool"=>array(
         "try"=>true,//发送错误重试次数,设置为TRUE为不限制
         "sleep"=>1,//断开连接重连暂停时间
         "master"=>array(
             "size"=>1,//队列长度
             "weight"=>1,//权重
             "connection"=>array(
-                'auth'              =>'foobaredOu9u080D8FM987H98H98N&%%&%',
+               // 'auth'              =>'xxx',
                 'host'             	=> '192.168.1.101',
                 'port'             	=> 6379,
                 'timeout'			=> '60',
@@ -72,6 +87,9 @@ return array(
         ),
     ),
     "postgresql"=>array(
+        'dsn' => 'host=127.0.0.1 port=5432 dbname=test user=root password=',
+    ),
+    "postgresql_pool"=>array(
         "try"=>true,//发送错误重试次数,设置为TRUE为不限制
         "sleep"=>1,//断开连接重连暂停时间
         "master"=>array(
